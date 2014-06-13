9fc2cd140c58228a100ace38b3af49a4b275ceaa
Jacinda Zhong <jacindaz@gmail.com>
2014-06-12 20:27:57 -0400

    Uses form_for for comments, creates controller for comments
     - form_for for comments on article shower page
     - comments controller saves the comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :articles, except: [:edit, :update, :destroy]
+  resources :articles do
+    resources :comments, only: :create
+  end
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +58,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
